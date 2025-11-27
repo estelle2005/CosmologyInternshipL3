12157d1371693941f16e7ae5e26d88fbba3ccd15
Eliot <kelcun.eliot@gmail.com>
2025-11-27 17:54:53 +0100

    code affiné, rajout de plusieurs courbes selon les paramètres

--- distances.py
+++ distances.py
@@ -3,50 +3,52 @@ import numpy as np
 from scipy.integrate import quad
 
 H_0 = 73.2
-
-omega_m = 0.3
 omega_r = 0.0001
-omega_lambda = 0.7
-
-#h = H_0 / 100
 coeff= 3*10**3
 
-def H(a, omega_m, omega_r, omega_lambda): # on sort le H_0
+
+def H(z, omega_m, omega_r, omega_lambda): # on sort le H_0
+    a = 1/(1+z)
     hubble_rate = np.sqrt(omega_m * a**-3 + omega_r * a**-4 + omega_lambda)
     return hubble_rate
 
-def function(z_prime):
-    a = 1/(1+z_prime)
-    return 1/H(a, omega_m, omega_r, omega_lambda)
+def invH(z_prime, omega_m, omega_r, omega_lambda):
+    return 1/H(z_prime, omega_m, omega_r, omega_lambda)
 
-def khi(z): #normalement c'est fonction de t mais t n'est pas ds l'expression et on trace khi(z) ??
-    res, err = quad(function, 0, z)
+def khi(z, omega_m, omega_r, omega_lambda):
+    res, err = quad(invH, 0, z, args = (omega_m, omega_r, omega_lambda))
     return res * coeff
 
-def d_A(z):
+def d_A(z, omega_m, omega_r, omega_lambda):
     a = 1 / (1+z)
-    return a * khi(z)
+    return a * khi(z, omega_m, omega_r, omega_lambda)
 
-def d_L(z) :
+def d_L(z, omega_m, omega_r, omega_lambda):
     a = 1 / (1+z)
-    return khi(z)/a
+    return khi(z, omega_m, omega_r, omega_lambda)/a
 
 
-z_min = 0
-z_max = 10
-z_values = np.linspace(z_min, z_max, 1000)
-
-khi_values = [khi(z) for z in z_values]
-d_A_values = [d_A(z) for z in z_values]
-d_L_values = [d_L(z) for z in z_values]
+z_values = np.linspace(0, 10, 1000)
+omega_m_list = [0.1, 0.3, 0.9]
 
 
-plt.plot(z_values, khi_values, 
-        linestyle='-', linewidth=2, label=f'$\chi$')
-plt.plot(z_values, d_A_values, 
-        linestyle='--', linewidth=2, label=f'$d_A$')
-plt.plot(z_values, d_L_values, 
-        linestyle='-.', linewidth=2, label=f'$d_L$')
+for i, om in enumerate(omega_m_list):
+    omega_lambda = 1 - om #univers plat
+    parameters = {'omega_m': om, 'omega_r': omega_r, 'omega_lambda': omega_lambda}
+    khi_values = [khi(z, **parameters)for z in z_values]
+    d_A_values = [d_A(z, **parameters)for z in z_values]
+    d_L_values = [d_L(z, **parameters)for z in z_values]
+    #H = H(z_values, om, omega_r, omega_lambda)
+    plt.plot(z_values, khi_values, 
+        linestyle='-', color=f'C{i}', linewidth=2, label=f'$\chi$; $\Omega_m$ = {om}; $\Omega_r$ = {omega_r}; $\Omega_\lambda$ = {omega_lambda}')
+    plt.plot(z_values, d_A_values, 
+        linestyle='--', color=f'C{i}', linewidth=2, label=f'$d_A$; $\Omega_m$ = {om}; $\Omega_r$ = {omega_r}; $\Omega_\lambda$ = {omega_lambda}')
+    plt.plot(z_values, d_L_values, 
+        linestyle='-.', color=f'C{i}', linewidth=2, label=f'$d_L$; $\Omega_m$ = {om}; $\Omega_r$ = {omega_r}; $\Omega_\lambda$ = {omega_lambda}')
+
+
+
+
 
 plt.xlabel("$z$")
 plt.ylabel("Distance [$h^{-1}$ Mpc]")
