5f3e1200d34328c462abd28b33a1ce934d4af89c
Eliot <kelcun.eliot@gmail.com>
2025-12-01 17:58:43 +0100

    définition des fonctions de plusieurs termes et de l'équation différentielle; correction des bugs

--- dark_energy_model.py
+++ dark_energy_model.py
@@ -8,10 +8,6 @@ Omega_m = 0.3
 Omega_Lambda = 1 - Omega_m
 
 ### $w(a) = w_0 + (1-a)w_a $
-### Compare $f(a)$ for :\n",
-
-
-"### Hint:  then the equation for $H^\\prime$, then write a new function to solve the differential equation"
 
 
 w_0_list = [-1.0, -1.2, -0.8, -1.0, -1.0]
@@ -32,14 +28,19 @@ for i, W_0 in enumerate(w_0_list):
         #on rajoute les coeffs
         return -H_0 * 3/2 * u_prime/u"""
 
-    """def Omega_m_a(a):
-        return Omega_m / (Omega_m + Omega_Lambda * a **-3)"""
+    def H_prime(a):
+        u_prime = Omega_m * a**(-3) + (1 + W_0 + W_a + a)*Omega_Lambda* a**(-3*(1 + W_0 + W_a))*np.exp(3*(1-a))
+        H_prime = - H_0 * 3/2 * u_prime / H(a)
+        return H_prime
+    
+    def Omega_m_a(a):
+        return Omega_m / (Omega_m + Omega_Lambda * a ** (-3*(W_0 + W_a)) * np.exp(3*W_a*(1-a)))
 
-    def dH_over_dlna(H, ln_a):
+    def df_over_dlna(f, ln_a):
         #f' = df/dlna
         a = np.exp(ln_a)
-        u_prime = Omega_m * a**(-3) + (1 + W_0 + W_a + a)*Omega_Lambda* a**(-3*(1 + W_0 + W_a ))*np.exp(3*(1-a))
-        deriv = - H_0 * 3/2 * u_prime / H(a)
+        #f' = - f² - \left( 2 + \frac{H\prime}{H} \right) f + \frac{3}{2}\Omega_m(a, w_a, w_0) 
+        deriv = -f**2 - (2 + H_prime(a)/H(a))*f + 1.5*Omega_m_a(a)
         return deriv
 
     #-- Definition of \"time\" = ln(a)
@@ -48,17 +49,16 @@ for i, W_0 in enumerate(w_0_list):
 
     #-- Initial condition - pour z bien supérieur à 1 (2)
     f0 = 1
-    f = odeint(dH_over_dlna, f0, ln_a)
+    f = odeint(df_over_dlna, f0, ln_a)
 
     plt.plot(a, f, 
-        linestyle='-', color=f'C{i}', linewidth=2, label=f'$\w_0$ = {W_0}; $\w_a$ = {W_a}')
+        linestyle='-', color=f'C{i}', linewidth=2, label=f'$w_0$ = {W_0}; $w_a$ = {W_a}')
 
 
 plt.xlabel('Scale factor a')
-plt.ylabel('Expansion rate H')
+plt.ylabel('Growth-rate f')
 plt.xscale('log')
 plt.grid(True)
-plt.title('Expansion rate with evolving dark energy')
 plt.legend()
 plt.tight_layout()
 plt.show()
