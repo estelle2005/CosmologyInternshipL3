acbcb1dc1bdcabe1fcdb1c97ae2343e49d898b36
Eliot <kelcun.eliot@gmail.com>
2025-12-01 11:55:13 +0100

    changement variable equation différentielle et début de définition des fonctions

--- equadiff.py
+++ equadiff.py
@@ -1,24 +1,66 @@
 import numpy as np
 from scipy.integrate import solve_ivp
 import matplotlib.pyplot as plt
+from scipy.integrate import odeint
 
 # Constantes cosmologiques
-H0 = 70  # km/s/Mpc
-Omega = 0.3
+H_0 = 70  # km/s/Mpc
+Omega_m = 0.3
+Omega_Lambda = 1 - 0.3
 
-# Fonction H(a) pour univers ΛCDM
+
+# Equation : f \prime + f^2 + \left( 2 + \frac{H\prime}{H} \right) f - \frac{3}{2}\frac{\Omega_m}{\Omega_m + \Omega_\lambda a^3} = 0
+
+
 def H(a):
-    Omega_Lambda = 1 - Omega
-    return H0 * np.sqrt(Omega / a**3 + Omega_Lambda)
+    return H_0 *np.sqrt(Omega_m * a**-3 + Omega_Lambda)
 
-# Coefficient a(a) = d(ln(a^3 H))/da
-def a_coeff(a):
-    dH_da = (H0**2 * -3*Omega/(2*a**4)) / H(a)  # dérivée de H(a)
-    return 3/a + dH_da / H(a)
+def H_prime_over_H(a):
+    #H'/H = dH/dlna /H = dlnH/dlna
+    H_prime = 
 
-# Coefficient b(a) = 3 Omega H0^2 / (2 a^5 H^2)
-def b_coeff(a):
-    return 3 * Omega * H0**2 / (2 * a**5 * H(a)**2)
+def Omega_m_a(a):
+    return Omega_m / (Omega_m + Omega_Lambda * a **-3)
+
+def df_over_dlna(f, ln_a):
+    #f' = df/dlna
+    a = np.exp(ln_a)
+    #f' = - f² - \left( 2 + \frac{H\prime}{H} \right) f + \frac{3}{2}\Omega_m(a) 
+    deriv = -f**2 - (2 + H_prime_over_H)*f + 1.5*Omega_m_a
+    return deriv
+
+#-- Definition of \"time\" = ln(a)
+a = 10.**np.linspace(-2, 0, 10000) #de 10**-2 à 10**0
+ln_a = np.log(a)
+#-- Initial condition pour z bien supérieur à 1 (2)
+f0 = 1
+f = odeint(df_over_dlna, f0, ln_a)
+
+    "#- Make the figure\n",
+    "\n",
+    "plt.figure()\n",
+    "plt.plot(a, f)\n",
+    "plt.xlabel('Scale factor a')\n",
+    "plt.ylabel('Growth-rate f')\n",
+    "plt.xscale('log')"
+
+
+    "### 3.2) In the cell below, make a figure comparing the growth-rate for several values of $\\Omega_m$. Describe what happens for increasingly large values of $\\Omega_m$. \n"
+    "### 3.3) Challenge (optional): solve and plot the solution for $f(a)$ for a different dark energy model, where its equation-of-state is given by \n",
+    "\n",
+    "### $w_{\\rm DE}(a) = w_0 + (1-a)w_a $\n",
+    "\n",
+    "### Compare $f(a)$ for :\n",
+    "\n",
+    "- $w_0 = -1.0$ and $w_a = 0$ (cosmological constant)\n",
+    "- $w_0 = -1.2$ and $w_a = 0$\n",
+    "- $w_0 = -0.8$ and $w_a = 0$\n",
+    "- $w_0 = -1.0$ and $w_a = 0.1$\n",
+    "- $w_0 = -1.0$ and $w_a = -0.1$\n",
+    "\n",
+    "### Hint: first find the equation for $H(a)$, then the equation for $H^\\prime$, then write a new function to solve the differential equation"
+
+"""
 
 # Système du premier ordre
 def dydA(a, yvec):
@@ -44,4 +86,6 @@ plt.xlabel("a")
 plt.ylabel("y(a)")
 plt.title("Solution numérique de l'équation différentielle")
 plt.grid(True)
-plt.show()
+plt.show()"""
+
+
