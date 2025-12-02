7c8251fd1c096345044d5e2b4d835b4c4bcb4e17
Estelle <estellechabre2005@gmail.com>
2025-12-02 16:43:18 +0100

    modification paramètres et dictionnaire

--- fonctions.py
+++ fonctions.py
@@ -12,32 +12,27 @@ a = 10.**np.linspace(-2, 0, 10000)  #de 10**-2 à 10**0
 ln_a = np.log(a)
 
 
-w_0_list = [-1, -1, -0.5, 0]
-w_a_list = [0, 0, 0, 0]
-Omega_Lambda_liste = [0.69, 0.72, 0.69, 0]
-
-
 def H(a, pars):
-    Omega_m = pars['Omega_m']
+    Omega_Lambda = pars['Omega_Lambda']
     W_0 = pars['W_0']
     W_a = pars['W_a']
-    Omega_Lambda = 1 - Omega_m
+    Omega_m = 1 - Omega_Lambda
     return H_0 *np.sqrt(Omega_m * a**-3 + Omega_Lambda*a**(-3*(1 + W_0 + W_a))*np.exp(-3*W_a*(1-a)))
 
 def H_prime(a, pars): #on définit un dictionnaire pour les paramètres
-    Omega_m = pars['Omega_m']
+    Omega_Lambda = pars['Omega_Lambda']
     W_0 = pars['W_0']
     W_a = pars['W_a']
-    Omega_Lambda = 1 - Omega_m
+    Omega_m = 1 - Omega_Lambda
     u_prime = Omega_m * a**(-3) + (1 + W_0 + W_a + a)*Omega_Lambda* a**(-3*(1 + W_0 + W_a))*np.exp(-3*W_a*(1-a))
     H_prime = - H_0 **2 * 3/2 * u_prime / H(a, pars)
     return H_prime
 
 def Omega_m_a(a, pars):
-    Omega_m = pars['Omega_m']
+    Omega_Lambda = pars['Omega_Lambda']
     W_0 = pars['W_0']
     W_a = pars['W_a']
-    Omega_Lambda = 1 - Omega_m
+    Omega_m = 1 - Omega_Lambda
     return Omega_m / (Omega_m + Omega_Lambda * a ** (-3*(W_0 + W_a)) * np.exp(-3*W_a*(1-a)))
 
 def df_over_dlna(f, ln_a, pars):
@@ -47,28 +42,29 @@ def df_over_dlna(f, ln_a, pars):
     deriv = -f**2 - (2 + H_prime(a, pars)/H(a, pars))*f + 1.5*Omega_m_a(a, pars)
     return deriv
 
-def growth_rate_f():
+def growth_rate_f(pars):
     f0 = 1 #condition initiale
-    f = odeint(df_over_dlna, f0, ln_a)
+    f = odeint(df_over_dlna, f0, ln_a, pars)
     return f
 
-def growth_factor_D():
+def growth_factor_D(pars):
     D_init = 0.01   #a_init = 0.01
     delta_lna = ln_a[1] - ln_a[0]
-    term = growth_rate_f() * delta_lna
+    term = growth_rate_f(pars) * delta_lna
     int_dlnD = np.cumsum(term)
     ln_D = int_dlnD + np.log(D_init)
     D = np.exp(ln_D)
     return D
 
 def plot_D():
-    plt.figure() 
-    for i, W_0 in enumerate(w_0_list):
-        W_a = w_a_list[i]
-        Omega_Lambda = Omega_Lambda_liste[i]
-        Omega_m = 1 - Omega_Lambda
-        plt.plot(a, growth_factor_D()/a, 
-            linestyle='-', color=f'C{i}', linewidth=2, label=f'$W$ = {W_0}; $\Omega_\Lambda$ = {Omega_Lambda}')
+    plt.figure()
+    w_0_list = [-1, -1, -0.5, 0]
+    w_a_list = [0, 0, 0, 0]
+    Omega_Lambda_list = [0.69, 0.72, 0.69, 0] 
+    for i in range (3):
+        pars = {'Omega_Lambda': Omega_Lambda_list[i], 'W_0': w_0_list[i], 'W_a': w_a_list[i]}  
+        plt.plot(a, growth_factor_D(pars)/a, 
+            linestyle='-', color=f'C{i}', linewidth=2, label=f'$W$ = {w_0_list[i]}; $\Omega_\Lambda$ = {Omega_Lambda_list[i]}')
     plt.xlabel('Scale factor a')
     plt.ylabel('Growth factor divided by a')
     plt.xscale('log')
@@ -77,3 +73,24 @@ def plot_D():
     plt.tight_layout()
     plt.show()
 
+
+def plot_f():
+    plt.figure()
+    w_0_list = [-1, -0.8, -0.6, -0.4, -0.2]
+    w_a_list = [0, -0.6, -1.2, -1.8, -2.4]
+    #Omega_m = 0.3
+    Omega_Lambda = 0.7
+    for i in range (3):
+        pars = {'Omega_Lambda': Omega_Lambda, 'W_0': w_0_list[i], 'W_a': w_a_list[i]}
+        f = growth_rate_f(pars)
+        plt.plot(a, f, 
+            linestyle='-', color=f'C{i}', linewidth=2, label=f'$W_0$ = {w_0_list[i]}; $w_a$ = {w_a_list[i]}')
+    plt.xlabel('Scale factor a')
+    plt.ylabel('Growth-rate f')
+    plt.xscale('log')
+    plt.grid(True)
+    plt.legend()
+    plt.tight_layout()
+    plt.show()
+
+plot_f()
