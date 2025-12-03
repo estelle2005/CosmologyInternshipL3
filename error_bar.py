10fcb474626bdc954cd4e98f4c73132006c4326d
Estelle <estellechabre2005@gmail.com>
2025-12-03 18:07:04 +0100

    lecture csv et definition fonctions theoriques Dv/r_d et DM / DH

--- error_bar.py
+++ error_bar.py
@@ -1,20 +1,18 @@
 import matplotlib.pyplot as plt
 import fonctions
 from scipy.stats import chisquare
+import pandas as pd
 
 
-H_values = fonctions.H(a,pars)
+
+"""H_values = fonctions.H(a,pars)
 a = 10.**np.linspace(-2, 0, 10000)  #de 10**-2 à 10**0
-ln_a = np.log(a)
+ln_a = np.log(a)"""
 #pars = {'Omega_Lambda': Omega_Lambda, 'W_0': W_0_list[i], 'W_a': W_a_list[i]}
 
 
 """Exemple : 
-# main.py
-import fonctions
-
 resultat = fonctions.addition(5, 3)
-print(resultat)  # 8
 print(fonctions.PI)  # 3.14159 import d'une variable globale
 """
 """# Effectifs observés et théoriques
@@ -26,3 +24,31 @@ chi2, p = chisquare(observes, theoriques)
 
 print(f"Statistique Khi carré : {chi2:.4f}")
 print(f"p-value : {p:.4f}")"""
+
+r_d = 147.05 # Mpc today
+c = 3 * 10**8
+
+tableau = pd.read_csv('DESI_DR2_BAO_measurements.csv')
+
+z = tableau['z_eff']
+
+def Dv_over_rd(z):
+    a = 1 / (1 + z)
+    D_M = fonctions.d_A(z, pars) * (1+z)
+    D_H = c / fonctions.H(a, pars)
+    Dv = (z * D_M**2 * D_H)**(1/3)
+    return Dv / r_d
+
+def DM_over_DH(z):
+    D_M = fonctions.d_A(z, pars) * (1+z)
+    D_H = c / fonctions.H(a, pars)
+    return D_M / D_H
+
+
+def chi_carré():
+    for z_i in z:
+
+pars = {'Omega_m': ,'Omega_Lambda': ,'W_'}
+
+iminuit
+Least
