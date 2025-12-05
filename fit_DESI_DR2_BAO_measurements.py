8ddf4a765c10d7e1ed38ac60e3b0c7a0762deaa2
Estelle <estellechabre2005@gmail.com>
2025-12-05 15:19:40 +0100

    plot fit pour DM over DH

--- fit_DESI_DR2_BAO_measurements.py
+++ fit_DESI_DR2_BAO_measurements.py
@@ -28,7 +28,6 @@ def Dv_over_rd(z_val, pars):
     Dv = (z_val * D_M**2 * D_H)**(1/3)
     return Dv / r_d
 
-
 def model_wrapper_Dv_over_rd(z_val, Omega_m, W_0, W_a, H_0):
     pars = {'Omega_m': Omega_m,'Omega_Lambda': 1 - Omega_m,'W_0': W_0, 'W_a': W_a, 'H_0': H_0}
     return [Dv_over_rd(z_i, pars) for z_i in z_val]
@@ -75,7 +74,6 @@ def iminuit_Dv_over_rd():
     plt.grid(True, alpha=0.3)
     plt.show()
     return m, pars_fit
-
 
 
 def DM_over_DH(z_val, pars):
@@ -268,11 +266,57 @@ def plot_fit_Dv_over_rd_error_bar():
     plt.show()
     return m, pars_fit
 
-plot_fit_Dv_over_rd_error_bar()
-plot_Dv_over_rd_double_()
+def plot_fit_DM_over_DH_error_bar():
+    cost = LeastSquares(z, DM_over_DH_exp, sigma_DM_over_DH, model_wrapper_DM_over_DH)
+    m = Minuit(cost, Omega_m=0.3, 
+               #Omega_Lambda = 0.7,
+               W_0 = -1, W_a = 0, H_0 = 73.2) 
+    
+    m.limits['Omega_m'] = (0.1, 1.0)
+    #m.limits['Omega_Lambda'] = (0.0, 1.0)
+    m.limits['W_0'] = (-2.0, 0.0)
+    m.limits['W_a'] = (-3.0, 2.0)
+    m.fixed['H_0'] = True
+
+    m.migrad()  # finds minimum of least_squares function
+    print("Résultat de l'ajustement:")
+    print(f"$\Omega_m$ = {m.values['Omega_m']:.3f} ± {m.errors['Omega_m']:.3f}")
+    #print(f"$\Omega_\Lambda$= {m.values['Omega_Lambda']:.3f} ± {m.errors['Omega_Lambda']:.3f}")
+    print(f"$w_0$ = {m.values['W_0']:.2f} ± {m.errors['W_0']:.2f}")
+    print(f"$w_a$= {m.values['W_a']:.2f} ± {m.errors['W_a']:.2f}")
+    print(f"χ²      = {m.fval:.2f}")
+    print(f"χ²/dof = {m.fval:.2f}/{m.ndof} = {m.fval/m.ndof:.2f}")
+
+    z_plot = np.linspace(min(z)*0.9, max(z)*1.1, 200)
+    pars_fit = {
+        'Omega_m': m.values['Omega_m'],
+        'Omega_Lambda': 1 - m.values['Omega_m'],
+        'W_0': m.values['W_0'],
+        'W_a': m.values['W_a'],
+        'H_0': m.values['H_0']}
+    
+    DM_plot = np.array([DM_over_DH(z_val, pars_fit) for z_val in z_plot])
+
+    fig, axs = plt.subplots(nrows=2, ncols=1)
+    axs[0].errorbar(z, DM_over_DH_exp, yerr=sigma_DM_over_DH, fmt='o', capsize=5,
+                label='Données BAO', color='darkblue')
+    axs[0].plot(z_plot, DM_plot, 'r-', linewidth=2,
+            label=f'Fit: $\Omega_m$={m.values["Omega_m"]:.3f}, $\Omega_\Lambda$= {pars_fit["Omega_Lambda"]:.3f},$w_0$={m.values["W_0"]:.2f}, $w_a$={m.values["W_a"]:.2f}')
+    axs[0].xlabel('Redshift z')
+    axs[0].ylabel(r'$D_M / D_H$')
+    axs[0].grid(True, alpha=0.3)
+    f_residu = [DM_over_DH(z_i, pars_fit) for z_i in z]
+    residu = ((DM_over_DH_exp - f_residu)/sigma_DM_over_DH)
+    axs[1].errorbar(z, residu, yerr=1, color='black', ecolor='red', fmt='o', label='Données ± erreur')
+    axs[1].set_xlabel('$z$')
+    axs[1].set_ylabel('Résidu normalisé')
+    axs[1].grid(True)
+    plt.show()
+    return m, pars_fit
 
 
 
 
 
-#graphique 2 : y _ y_model / sigma_y
+
+
