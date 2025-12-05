23f468efb8c64b83d3bc3459ad5c8c9260cd702c
Estelle <estellechabre2005@gmail.com>
2025-12-05 17:13:38 +0100

    double plot

--- DESI_DR1__measurements.py
+++ DESI_DR1__measurements.py
@@ -75,4 +75,58 @@ def iminuit_fsigma8():
     plt.show()
     return m, pars_fit
 
-iminuit_fsigma8()
+def plot_fit_fsigma8_error_bar():
+    cost = LeastSquares(z, fsigma8_exp, sigma_fsigma8, model_wrapper_fsigma8)
+    m = Minuit(cost, Omega_m=0.3, 
+               #Omega_Lambda = 0.7,
+               W_0 = -1, W_a = 0, H_0 = 73.2, sigma8 = 0.8) 
+    
+    m.limits['Omega_m'] = (0.1, 1.0)
+    #m.limits['Omega_Lambda'] = (0.0, 1.0)
+    m.limits['W_0'] = (-2.0, 0.0)
+    m.limits['W_a'] = (-3.0, 2.0)
+    m.fixed['H_0'] = True
+    m.limits['sigma8'] = (0.6, 1.0)
+
+    m.migrad()  # finds minimum of least_squares function
+    print("Résultat de l'ajustement:")
+    print(f"$\Omega_m$ = {m.values['Omega_m']:.3f} ± {m.errors['Omega_m']:.3f}")
+    #print(f"$\Omega_\Lambda$= {m.values['Omega_Lambda']:.3f} ± {m.errors['Omega_Lambda']:.3f}")
+    print(f"$w_0$ = {m.values['W_0']:.2f} ± {m.errors['W_0']:.2f}")
+    print(f"$w_a$= {m.values['W_a']:.2f} ± {m.errors['W_a']:.2f}")
+    print(f"$f_(\sigma 8)$ = {m.values['sigma8']:.2f} ± {m.errors['sigma8']:.2f}")
+    print(f"χ²      = {m.fval:.2f}")
+    print(f"χ²/dof = {m.fval:.2f}/{m.ndof} = {m.fval/m.ndof:.2f}")
+
+    z_plot = np.linspace(min(z)*0.9, max(z)*1.1, 200)
+    pars_fit = {
+        'Omega_m': m.values['Omega_m'],
+        'Omega_Lambda': 1 - m.values['Omega_m'],
+        'W_0': m.values['W_0'],
+        'W_a': m.values['W_a'],
+        'H_0': m.values['H_0'],
+        'sigma8': m.values['sigma8']
+        }
+    
+    fsigma8_plot = np.array([fsigma8_th(z_val, pars_fit) for z_val in z_plot])
+
+    fig, axs = plt.subplots(nrows=2, ncols=1, figsize= (8,6))
+    axs[0].errorbar(z, fsigma8_exp, yerr=sigma_fsigma8, fmt='o', capsize=5,
+                label='Données BAO', color='darkblue')
+    axs[0].plot(z_plot, fsigma8_plot, 'r-', linewidth=2,
+            label=f'Fit: $\Omega_m$={m.values["Omega_m"]:.3f}, $\Omega_\Lambda$= {pars_fit["Omega_Lambda"]:.3f},$w_0$={m.values["W_0"]:.2f}, $w_a$={m.values["W_a"]:.2f}, $f_\sigma8$={pars_fit["sigma8"]}')
+    axs[0].set_ylabel(r'$f_{\sigma8}$')
+    axs[0].legend()
+    axs[0].grid(True, alpha=0.3)
+    f_residu = [fsigma8_th(z_i, pars_fit) for z_i in z]
+    residu = ((fsigma8_exp - f_residu)/sigma_fsigma8)
+    axs[1].errorbar(z, residu, yerr=1, color='black', ecolor='red', fmt='o', label='Données ± erreur')
+    axs[1].set_xlabel('$z$')
+    axs[1].set_ylabel('Résidu normalisé')
+    axs[1].grid(True)
+    axs[1].legend()
+    plt.savefig('/home/etudiant15/Documents/STAGE CPPM/Figures/f_sigma8_DESI_DR1_double.pdf', bbox_inches='tight')
+    plt.show()
+    return m, pars_fit
+
+plot_fit_fsigma8_error_bar()
