2eca0afa2ce57d82ca3c092777b5b8d1b78f6d76
Estelle <estellechabre2005@gmail.com>
2025-12-08 17:49:35 +0100

    correction label

--- DESI_DR1__measurements.py
+++ DESI_DR1__measurements.py
@@ -69,7 +69,7 @@ def iminuit_fsigma8():
     plt.errorbar(z, fsigma8_exp, yerr=sigma_fsigma8, fmt='o', capsize=5,
                 label='RSD Data', color='darkblue')
     plt.plot(z_plot, fsigma8_plot, 'r-', linewidth=2,
-            label=f'Fit: $\Omega_m$={m.values["Omega_m"]:.3f}, $\Omega_\Lambda$= {pars_fit["Omega_Lambda"]:.3f},$w_0$={m.values["W_0"]:.2f}, $w_a$={m.values["W_a"]:.2f}, $f_\sigma8$={pars_fit["sigma8"]}')
+            label=f'Fit: $\Omega_m$={m.values["Omega_m"]:.3f}, $\Omega_\Lambda$= {pars_fit["Omega_Lambda"]:.3f},$w_0$={m.values["W_0"]:.2f}, $w_a$={m.values["W_a"]:.2f}, $\sigma_8$={pars_fit["sigma8"]}')
     plt.xlabel('Redshift z')
     plt.ylabel(r'$f_{\sigma8}$')
     plt.legend()
@@ -118,13 +118,13 @@ def plot_fit_fsigma8_error_bar():
     axs[0].errorbar(z, fsigma8_exp, yerr=sigma_fsigma8, fmt='o', capsize=5,
                 label='RSD Data', color='darkblue')
     axs[0].plot(z_plot, fsigma8_plot, 'r-', linewidth=2,
-            label=f'Fit: $\Omega_m$={m.values["Omega_m"]:.3f}, $\Omega_\Lambda$= {pars_fit["Omega_Lambda"]:.3f},$w_0$={m.values["W_0"]:.2f}, $w_a$={m.values["W_a"]:.2f}, $f_\sigma8$={pars_fit["sigma8"]}')
+            label=f'Fit: $\Omega_m$={m.values["Omega_m"]:.3f}, $\Omega_\Lambda$= {pars_fit["Omega_Lambda"]:.3f},$w_0$={m.values["W_0"]:.2f}, $w_a$={m.values["W_a"]:.2f}, $\sigma_8$={pars_fit["sigma8"]}')
     axs[0].set_ylabel(r'$f_{\sigma8}$')
     axs[0].legend()
     axs[0].grid(True, alpha=0.3)
     f_residu = [fsigma8_th(z_i, pars_fit) for z_i in z]
     residu = ((fsigma8_exp - f_residu)/sigma_fsigma8)
-    axs[1].errorbar(z, residu, yerr=1, color='black', ecolor='red', fmt='o', label='RSD Data')
+    axs[1].errorbar(z, residu, yerr=1, color='black', ecolor='red', fmt='o', capsize=5, label='RSD Data')
     axs[1].set_xlabel('$z$')
     axs[1].set_ylabel('Normalized residue')
     axs[1].grid(True)
@@ -132,3 +132,5 @@ def plot_fit_fsigma8_error_bar():
     plt.savefig('/home/etudiant15/Documents/STAGE CPPM/Figures/f_sigma8_DESI_DR1_double.pdf', bbox_inches='tight')
     plt.show()
     return m, pars_fit
+
+plot_fit_fsigma8_error_bar()
