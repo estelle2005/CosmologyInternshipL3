6bb5f0f50e9be3a138ff4b9dfe0d610357e11d48
Estelle <estellechabre2005@gmail.com>
2025-12-10 10:51:39 +0100

    modele w_a fixé pour wCDM

--- fit_DESI_DR2_BAO_measurements.py
+++ fit_DESI_DR2_BAO_measurements.py
@@ -350,8 +350,7 @@ def plot_fit_DM_over_DH_error_bar():
     return m, pars_fit
 
 
-#KHI CARRE QUI SOMME 
-
+#ΛCDM
 def plot_fit_combined():
     cost_DM = LeastSquares(z_DM, DM_over_DH_exp, sigma_DM_over_DH, model_wrapper_DM_over_DH)
     cost_Dv = LeastSquares(z_Dv, DV_over_rd_exp, sigma_DV_over_rd, model_wrapper_Dv_over_rd)
@@ -533,6 +532,207 @@ def plot_fit_combined_error_bar():
         pars_fit['W_a']]
     for i in range(len(parameters)):
         print(m.params[i]('merror'))"""
+    
+    merrors_m = m.merrors["Omega_m"]
+    lower_m = merrors_m.lower
+    upper_m = merrors_m.upper
+    inf_m = - lower_m
+
+    merrors_0 = m.merrors["W_0"]
+    lower_0 = merrors_0.lower
+    upper_0 = merrors_0.upper
+    inf_0 = - lower_0
+
+    merrors_a = m.merrors["W_a"]
+    lower_a = merrors_a.lower
+    upper_a = merrors_a.upper
+    inf_a = - lower_a
+
+    merrors_H = m.merrors["H_0xr_d"]
+    lower_H = merrors_H.lower
+    upper_H = merrors_H.upper
+    inf_H = - lower_H
+    #ON PEUT AUSSI FAIRE UN DICTIONNAIRE AVEC LES ERREURS DEDANS
+
+    print(f'BAO & ${m.values["Omega_m"]:.3f}^{{+{upper_m:.3f}}}_{{{- inf_m:.3f}}}$ & ${m.values["W_0"]:.3f}^{{+{upper_0:.3f}}}_{{{- inf_0:.3f}}}$ & ${m.values["W_a"]:.3f}^{{+{upper_a:.3f}}}_{{{- inf_a:.3f}}}$ & - & ${m.values["H_0xr_d"]:.3f}^{{+{upper_H:.3f}}}_{{{- inf_H:.3f}}}$')
+    return m, pars_fit
+
+#wCDM
+def plot_fit_combined_wCDM():
+    cost_DM = LeastSquares(z_DM, DM_over_DH_exp, sigma_DM_over_DH, model_wrapper_DM_over_DH)
+    cost_Dv = LeastSquares(z_Dv, DV_over_rd_exp, sigma_DV_over_rd, model_wrapper_Dv_over_rd)
+    combined_cost = cost_DM + cost_Dv
+
+    m = Minuit(combined_cost, Omega_m=0.3, 
+               #Omega_Lambda = 0.7,
+               W_0 = -1, W_a = 0, H_0 = 73.2, H_0xr_d = 10764.06) 
+    
+    m.limits['Omega_m'] = (0.1, 1.0)
+    #m.limits['Omega_Lambda'] = (0.0, 1.0)
+    m.limits['W_0'] = (-2.0, 0.0)
+    m.fixed['W_a'] = True
+    m.fixed['H_0'] = True
+    m.limits['H_0xr_d'] = (5000, 20000)
+
+    m.migrad()  # finds minimum of least_squares function
+    m.minos()
+    m.draw_mncontour("Omega_m", "W_0", cl=(0.683, 0.954, 0.997), size=300)
+    print(m)
+    print("Résultat de l'ajustement:")
+    print(f"$\Omega_m$ = {m.values['Omega_m']:.3f} ± {m.errors['Omega_m']:.3f}")
+    #print(f"$\Omega_\Lambda$= {m.values['Omega_Lambda']:.3f} ± {m.errors['Omega_Lambda']:.3f}")
+    print(f"$w_0$ = {m.values['W_0']:.2f} ± {m.errors['W_0']:.2f}")
+    print(f"$w_a$= {m.values['W_a']:.2f} ± {m.errors['W_a']:.2f}")
+    print(f"$H_0$= {m.values['H_0']:.2f} ± {m.errors['H_0']:.2f}")
+    print(f"$H_0$= {m.values['H_0']:.2f} ± {m.errors['H_0']:.2f}")
+    print(f"$H_0xr_d$= {m.values['H_0xr_d']:.2f} ± {m.errors['H_0xr_d']:.2f}")
+    print(f"χ²      = {m.fval:.2f}")
+    print(f"χ²/dof = {m.fval:.2f}/{m.ndof} = {m.fval/m.ndof:.2f}")
+
+    pars_fit = {
+        'Omega_m': m.values['Omega_m'],
+        'Omega_Lambda': 1 - m.values['Omega_m'],
+        'W_0': m.values['W_0'],
+        'W_a': m.values['W_a'],
+        'H_0': m.values['H_0'],
+        'H_0xr_d': m.values['H_0xr_d']}
+    
+    chi2_DM = np.sum(((DM_over_DH_exp - model_wrapper_DM_over_DH(z_DM, *m.values)) / sigma_DM_over_DH)**2)
+    chi2_DV = np.sum(((DV_over_rd_exp - model_wrapper_Dv_over_rd(z_Dv, *m.values)) / sigma_DV_over_rd)**2)
+    
+    print(f"\nDétail des χ²:")
+    print(f"  χ²_DM/DH = {chi2_DM:.2f}")
+    print(f"  χ²_DV/rd = {chi2_DV:.2f}")
+    print(f"  Somme vérifiée = {chi2_DM + chi2_DV:.2f}")
+
+    fig, axs = plt.subplots(nrows=2, ncols=1, figsize= (8,6))
+
+    z_plot_DM = np.linspace(min(z_DM)*0.9, max(z_DM)*1.1, 200)
+    DM_plot = np.array([DM_over_DH(z_val, pars_fit) for z_val in z_plot_DM])
+    axs[0].errorbar(z_DM, DM_over_DH_exp, yerr=sigma_DM_over_DH, fmt='o', capsize=5,
+                label='BAO Data', color='darkblue')
+    axs[0].plot(z_plot_DM, DM_plot, 'r-', linewidth=2,
+            label=f'Fit: $\Omega_m$={m.values["Omega_m"]:.3f}, $\Omega_\Lambda$= {pars_fit["Omega_Lambda"]:.3f},$w_0$={m.values["W_0"]:.2f}, $w_a$={m.values["W_a"]:.2f},$H_0r_d$={m.values["H_0xr_d"]:.2f}')
+    axs[0].set_xlabel('Redshift $z$')
+    axs[0].set_ylabel(r'$D_M / D_H$')
+    axs[0].grid(True, alpha=0.3)
+    axs[0].legend()
+
+    z_plot_DV = np.linspace(min(z_Dv)*0.9, max(z_Dv)*1.1, 200)
+    DV_plot = np.array([Dv_over_rd(z_val, pars_fit) for z_val in z_plot_DV])
+    axs[1].errorbar(z_Dv, DV_over_rd_exp, yerr=sigma_DV_over_rd, fmt='o', capsize=5,
+                label='BAO Data', color='darkgreen')
+    axs[1].plot(z_plot_DV, DV_plot, 'r-', linewidth=2,
+            label=f'Fit: $\Omega_m$={m.values["Omega_m"]:.3f}, $\Omega_\Lambda$= {pars_fit["Omega_Lambda"]:.3f},$w_0$={m.values["W_0"]:.2f}, $w_a$={m.values["W_a"]:.2f}, ,$H_0r_d$={m.values["H_0xr_d"]:.2f}')
+    axs[1].set_xlabel('Redshift $z$')
+    axs[1].set_ylabel(r'$D_v / r_d$')
+    axs[1].grid(True, alpha=0.3)
+    axs[1].legend()
+
+    plt.savefig('/home/etudiant15/Documents/STAGE CPPM/Figures/chi_combined_DESI_DR2_wCDM.pdf', bbox_inches='tight')
+    plt.show()
+    return m, pars_fit
+
+def plot_fit_combined_error_bar_wCDM():
+    cost_DM = LeastSquares(z_DM, DM_over_DH_exp, sigma_DM_over_DH, model_wrapper_DM_over_DH)
+    cost_Dv = LeastSquares(z_Dv, DV_over_rd_exp, sigma_DV_over_rd, model_wrapper_Dv_over_rd)
+    combined_cost = cost_DM + cost_Dv
+
+    m = Minuit(combined_cost, Omega_m=0.3, 
+               #Omega_Lambda = 0.7,
+               W_0 = -1, W_a = 0, H_0 = 73.2, H_0xr_d = 10764.06) 
+    
+   
+    m.limits['Omega_m'] = (0.1, 1.0)
+    #m.limits['Omega_Lambda'] = (0.0, 1.0)
+    m.limits['W_0'] = (-3.0, 1.0)
+    m.fixed['W_a'] = True
+    m.fixed['H_0'] = True
+    m.limits['H_0xr_d'] = (5000, 20000)
+
+    
+    """#ON FIXE W_a et H_0 n'est plus fixe
+    m.limits['Omega_m'] = (0.1, 1.0)
+    #m.limits['Omega_Lambda'] = (0.0, 1.0)
+    m.limits['W_0'] = (-3.0, 1.0)
+    #m.limits['W_a'] = (-3.0, 2.0)
+    m.fixed['W_a'] = True
+    m.fixed['H_0'] = False
+    m.limits['H_0']= (50, 150)"""
+
+    m.migrad() # finds minimum of least_squares function
+    m.minos() 
+    m.draw_mncontour("Omega_m", "W_0", cl=(0.683, 0.954, 0.997), size=300)
+
+    print(m)
+    print("Résultat de l'ajustement:")
+    print(f"$\Omega_m$ = {m.values['Omega_m']:.3f} ± {m.errors['Omega_m']:.3f}")
+    #print(f"$\Omega_\Lambda$= {m.values['Omega_Lambda']:.3f} ± {m.errors['Omega_Lambda']:.3f}")
+    print(f"$w_0$ = {m.values['W_0']:.2f} ± {m.errors['W_0']:.2f}")
+    print(f"$w_a$= {m.values['W_a']:.2f} ± {m.errors['W_a']:.2f}")
+    print(f"$H_0$= {m.values['H_0']:.2f} ± {m.errors['H_0']:.2f}")
+    print(f"$H_0xr_d$= {m.values['H_0xr_d']:.2f} ± {m.errors['H_0xr_d']:.2f}")
+    print(f"χ²      = {m.fval:.2f}")
+    print(f"χ²/dof = {m.fval:.2f}/{m.ndof} = {m.fval/m.ndof:.2f}")
+
+    pars_fit = {
+        'Omega_m': m.values['Omega_m'],
+        'Omega_Lambda': 1 - m.values['Omega_m'],
+        'W_0': m.values['W_0'],
+        'W_a': m.values['W_a'],
+        'H_0': m.values['H_0'],
+        'H_0xr_d': m.values['H_0xr_d']}
+    
+    chi2_DM = np.sum(((DM_over_DH_exp - model_wrapper_DM_over_DH(z_DM, *m.values)) / sigma_DM_over_DH)**2)
+    chi2_DV = np.sum(((DV_over_rd_exp - model_wrapper_Dv_over_rd(z_Dv, *m.values)) / sigma_DV_over_rd)**2)
+    
+    print(f"\nDétail des χ²:")
+    print(f"  χ²_DM/DH = {chi2_DM:.2f}")
+    print(f"  χ²_DV/rd = {chi2_DV:.2f}")
+    print(f"  Somme vérifiée = {chi2_DM + chi2_DV:.2f}")
+
+    fig, axs = plt.subplots(nrows=4, ncols=1, figsize= (9,8))
+
+    z_plot_DM = np.linspace(min(z_DM)*0.9, max(z_DM)*1.1, 200)
+    DM_plot = np.array([DM_over_DH(z_val, pars_fit) for z_val in z_plot_DM])
+    axs[0].errorbar(z_DM, DM_over_DH_exp, yerr=sigma_DM_over_DH, fmt='o', capsize=5,
+                label='BAO Data', color='darkblue')
+    axs[0].plot(z_plot_DM, DM_plot, 'r-', linewidth=2,
+            label=f'Fit: $\Omega_m$={m.values["Omega_m"]:.3f}, $\Omega_\Lambda$= {pars_fit["Omega_Lambda"]:.3f},$w_0$={m.values["W_0"]:.2f}, $w_a$={m.values["W_a"]:.2f},$H_0r_d$={m.values["H_0xr_d"]:.2f}')
+    axs[0].set_xlabel('Redshift $z$')
+    axs[0].set_ylabel(r'$D_M / D_H$')
+    axs[0].grid(True, alpha=0.3)
+    axs[0].legend()
+
+    f_residu_DM = [DM_over_DH(z_i, pars_fit) for z_i in z_DM]
+    residu = ((DM_over_DH_exp - f_residu_DM)/sigma_DM_over_DH)
+    axs[1].errorbar(z_DM, residu, yerr=1, color='black', ecolor='red', fmt='o', capsize=5, label='BAO Data')
+    axs[1].set_xlabel('$z$')
+    axs[1].set_ylabel('Normalized residue')
+    axs[1].grid(True)
+    axs[1].legend()
+
+    z_plot_DV = np.linspace(min(z_Dv)*0.9, max(z_Dv)*1.1, 200)
+    DV_plot = np.array([Dv_over_rd(z_val, pars_fit) for z_val in z_plot_DV])
+    axs[2].errorbar(z_Dv, DV_over_rd_exp, yerr=sigma_DV_over_rd, fmt='o', capsize=5,
+                label='BAO Data', color='darkgreen')
+    axs[2].plot(z_plot_DV, DV_plot, 'r-', linewidth=2,
+            label=f'Fit: $\Omega_m$={m.values["Omega_m"]:.3f}, $\Omega_\Lambda$= {pars_fit["Omega_Lambda"]:.3f},$w_0$={m.values["W_0"]:.2f}, $w_a$={m.values["W_a"]:.2f},$H_0r_d$={m.values["H_0xr_d"]:.2f}')
+    axs[2].set_xlabel('Redshift $z$')
+    axs[2].set_ylabel(r'$D_v / r_d$')
+    axs[2].grid(True, alpha=0.3)
+    axs[2].legend()
+
+    f_residu_DV = [Dv_over_rd(z_i, pars_fit) for z_i in z_Dv]
+    residu = ((DV_over_rd_exp - f_residu_DV)/sigma_DV_over_rd)
+    axs[3].errorbar(z_Dv, residu, yerr=1, color='black', ecolor='red', fmt='o', capsize=5, label='BAO Data')
+    axs[3].set_xlabel('$z$')
+    axs[3].set_ylabel('Normalized residue')
+    axs[3].grid(True)
+    axs[3].legend()
+
+    plt.savefig('/home/etudiant15/Documents/STAGE CPPM/Figures/chi_combined_DESI_DR2_error_bar_wCDM.pdf', bbox_inches='tight')
+    plt.show()
     
     merrors_m = m.merrors["Omega_m"]
     lower_m = merrors_m.lower
