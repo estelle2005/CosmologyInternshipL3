7898fd6d38af7c8f360a2531f783a850e90f6fc6
Estelle <estellechabre2005@gmail.com>
2025-12-12 16:04:19 +0100

    plot MCMC

--- DR1_DR2_MCMC.py
+++ DR1_DR2_MCMC.py
@@ -5,6 +5,7 @@ import pandas as pd
 from iminuit.cost import LeastSquares
 from matplotlib.lines import Line2D
 import emcee
+import corner
 
 r_d = 147.05  # Mpc today
 c = 3 * 10**5  # en km
@@ -37,15 +38,14 @@ fsigma8_exp_noPV = tableau_DR1_noPV["fsigma8"].to_numpy()
 sigma_fsigma8_noPV = tableau_DR1_noPV["fsigma8_err"].to_numpy()
 
 para_names = ["Omega_m", "W_0", "W_a", "sigma8", "H_0xr_d"]
-
-def chi_carré_Dv_over_rd(pars):
+ndim = len(para_names)
+"""def chi_carré_Dv_over_rd(pars):
     sum = 0
     for i in range(len(z_Dv)):
         sum += ((DV_over_rd_exp[i] - fonctions.Dv_over_rd(z_Dv[i], pars)) ** 2) / (
             (sigma_DV_over_rd[i]) ** 2
         )
     return sum
-
 
 def chi_carré_DM_over_DH(pars):
     sum = 0
@@ -54,20 +54,64 @@ def chi_carré_DM_over_DH(pars):
             (sigma_DM_over_DH[i]) ** 2
         )
     return sum
+"""
+
+def chi_carré_Dv_over_rd(pars):
+    sum = 0
+    for i in range(len(z_Dv)):
+        theo_val = fonctions.Dv_over_rd(z_Dv[i], pars)
+        if np.isnan(theo_val) or np.isinf(theo_val):
+            return np.inf
+        
+        exp_val = DV_over_rd_exp[i]
+        sigma = sigma_DV_over_rd[i]
+
+        if np.isnan(exp_val) or sigma <= 0:
+            return np.inf
+
+        sum += ((exp_val - theo_val) ** 2) / (sigma ** 2)
+    return sum
+
+def chi_carré_DM_over_DH(pars):
+    sum = 0
+    for i in range(len(z_DM)):
+        theo_val = fonctions.DM_over_DH(z_DM[i], pars)
+        if np.isnan(theo_val) or np.isinf(theo_val):
+            return np.inf
+        
+        exp_val = DM_over_DH_exp[i]
+        sigma = (sigma_DM_over_DH[i])
+
+        sum += ((exp_val - theo_val) ** 2) / (sigma ** 2)
+    return sum
 
 
-def chi_carré(pars):
+#W_0_list = [-1, -0.6, -0.2]
+#W_a_list = [0, -1.2, -2.4]
+#Omega_m_list = [0.1, 0.3, 0.9]
+#for i in range(len(Omega_m_list)):
+#    pars = {"Omega_m": Omega_m_list[i],
+#            "Omega_Lambda": 1 - Omega_m_list[i],
+#            "W_0": W_0_list[i],
+#            "W_a": W_a_list[i],
+#            "H_0": 73.2, "H_0xr_d":10764.06,}
+#    chi_carré_DM_over_DH(pars)
+#    chi_carré_Dv_over_rd(pars)
+#
+#QUE DR2
+
+def chi_carré_BAO(pars):
     return chi_carré_DM_over_DH(pars) + chi_carré_Dv_over_rd(pars)
 
-def log_prior(p, limits):
+def log_prior_BAO(p, limits):
     for i, param in enumerate(para_names):
-        if p[i] < limits[param][0] or p[i] > limits[param][1]:
+        low, high = limits[param]
+        if p[i] < low or p[i] > high:
             return -np.inf
-    else : 
+    else:
         return 0
  
-    return 0
-def log_prob(p, limits):
+def log_prob_BAO(p, limits):
     pars = {
         "Omega_m": p[0],
         #"Omega_Lambda": 1 - p[0],
@@ -77,13 +121,11 @@ def log_prob(p, limits):
         "sigma8": p[3],
         "H_0xr_d": p[4],
     }
-    return -chi_carré(pars) / 2 + log_prior(p, limits)
+    return -chi_carré_BAO(pars) / 2 + log_prior_BAO(p, limits)
 
 
-def mcmc_BAO_RSD_PV_w0wa():
+def mcmc_BAO_w0wa():
     nwalkers = 10
-    ndim = len(para_names)
-
     limits = {}
     limits["Omega_m"] = (0.1, 1.0)
     limits["W_0"] = (-3.0, 1.0)
@@ -105,22 +147,51 @@ def mcmc_BAO_RSD_PV_w0wa():
     for j in range(ndim):
         max = p0[:,j].max()
         min = p0[:,j].min()
-        print("Le max du paramètre", para_names[j], "est :", max)
-        print("Le min du paramètre", para_names[j], "est :", min)
+        #print("Le max du paramètre", para_names[j], "est :", max)
+        #print("Le min du paramètre", para_names[j], "est :", min)
 
     #print("p0", p0.shape)
-    sampler = emcee.EnsembleSampler(nwalkers, ndim, log_prob, args=[limits])
+    sampler = emcee.EnsembleSampler(nwalkers, ndim, log_prob_BAO, args=[limits])
     #log_prob(p0[0], limits)
     #print(log_prob(p0[0], limits))
-    state = sampler.run_mcmc(p0, 100)
-    sampler.run_mcmc(state, 10000)
-    """sampler.reset()
-    sampler.run_mcmc(state, 10000)
+    state = sampler.run_mcmc(p0, 10000)
+    sampler.run_mcmc(state, 100, progress=True)
     samples = sampler.get_chain(flat=True)
-    plt.hist(samples[:, 0], 100, color="k", histtype="step")
-    plt.xlabel(r"$\theta_1$")
-    plt.ylabel(r"$p(\theta_1)$")
-    plt.gca().set_yticks([]);"""
+    np.save('mes_chaines_BAO_w0wa.npy', samples)
+    """sampler.reset()
+    sampler.run_mcmc(state, 10000)"""
 
-mcmc_BAO_RSD_PV_w0wa()
+def plot_mcmc_BAO_w0wa():
+    samples = np.load('mes_chaines_BAO_w0wa.npy')
+    n_cols = samples.shape[1]
+    #fig, axes = plt.subplots(5, figsize=(10, 10), sharex=True)
+    #print(f"Shape de samples: {samples.shape}")
+    #print(f"Nombre d'échantillons: {len(samples)}")
+    #print("n_cols:", n_cols)
+    #print("ndim:", ndim)
+    """for i in range(ndim):
+        ax = axes[i]
+        ax.plot(samples[:, i], "k", alpha=0.3)
+        ax.set_xlim(0, len(samples))
+        ax.set_ylabel(para_names[i])
+        ax.yaxis.set_label_coords(-0.1, 0.5)
+    axes[-1].set_xlabel("step number")"""
+    fig = corner.corner(samples[:, :5], labels=para_names, )
+    plt.savefig(
+        "/home/etudiant15/Documents/STAGE CPPM/Figures/MCMC_BAO_w0waCDM.pdf",
+        bbox_inches="tight",
+    )
+    plt.tight_layout()
+    plt.show()
+
+""" fig = corner.corner(
+        samples[:, :5],  # Prend seulement les 5 premières colonnes
+        labels=para_names,
+        show_titles=True,
+        plot_datapoints=False
+    )"""
+  
+
+mcmc_BAO_w0wa()
 # p defini avc paramètres dans l'ordre
+#faire apres avec DR1 avec avec et sans PV et w0waCDM et wCDM
